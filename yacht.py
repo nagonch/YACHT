92fffb4bc9f2f9d47521832c5006036f5d0a6131
Nikolai Goncharov <kolchindru@gmail.com>
2025-04-07 10:46:56 +1000

    add config

--- yacht.py
+++ yacht.py
@@ -1,5 +1,10 @@
 import numpy as np
 from PIL import Image
+import yaml
+
+with open("config.yaml") as file:
+    CONFIG = yaml.safe_load(file)
 
 if __name__ == "__main__":
+    print(CONFIG)
     pass
