5dcf275dd6c997b8e26db6cd2055ebdd9b6ac7bd
Nikolai Goncharov <kolchindru@gmail.com>
2025-04-15 11:27:30 +1000

    fix

--- handeye_cal.py
+++ handeye_cal.py
@@ -115,31 +115,4 @@ def get_cam_to_arm(arm_to_base_rotation, arm_to_base_translation, camera_params)
 
 
 if __name__ == "__main__":
-    arm_to_base_rotation = np.load("arm_to_base_rotation.npy")
-    arm_to_base_translation = np.load("arm_to_base_translation.npy")
-    target_to_cam_rotation = np.load("target_to_cam_rotation.npy")
-    target_to_cam_translation = np.load("target_to_cam_translation.npy")
-    target_to_base_pose = np.array([0, 0, 0, 0, 0, 0, 1])
-    cam_to_arm_pose = np.array([0, 0, 0, 0, 0, 0, 1])
-    x = np.concatenate((target_to_base_pose, cam_to_arm_pose))
-    result = least_squares(
-        get_loss,
-        x,
-        args=(
-            arm_to_base_rotation,
-            arm_to_base_translation,
-            target_to_cam_rotation,
-            target_to_cam_translation,
-        ),
-    )
-    x_opt = result.x
-    target_to_base_pose = x_opt[:7]
-    cam_to_arm_pose = x_opt[7:]
-
-    target_to_base_T = np.eye(4)
-    target_to_base_T[:3, :3] = R.from_quat(target_to_base_pose[3:]).as_matrix()
-    target_to_base_T[:3, 3] = target_to_base_pose[:3]
-    cam_to_arm_T = np.eye(4)
-    cam_to_arm_T[:3, :3] = R.from_quat(cam_to_arm_pose[3:]).as_matrix()
-    cam_to_arm_T[:3, 3] = cam_to_arm_pose[:3]
-    print(cam_to_arm_T)
+    pass
