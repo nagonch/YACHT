5e69c77b70c5ec7fcc72996ef28edc00d8be0cad
Nikolai Goncharov <kolchindru@gmail.com>
2025-04-15 10:10:54 +1000

    fix

--- handeye_cal.py
+++ handeye_cal.py
@@ -21,8 +21,24 @@ def get_loss(
     cam_to_arm_T = np.eye(4)
     cam_to_arm_T[:3, :3] = R.from_quat(cam_to_arm_pose[3:]).as_matrix()
     cam_to_arm_T[:3, 3] = cam_to_arm_pose[:3]
-    print(cam_to_arm_T)
-    print(target_to_base_T)
+
+    arm_to_base_T = np.stack(
+        [
+            np.eye(4),
+        ]
+        * arm_to_base_rotation.shape[0]
+    )
+    arm_to_base_T[:, :3, :3] = arm_to_base_rotation
+    arm_to_base_T[:, :3, 3] = arm_to_base_translation
+
+    target_to_cam_T = np.stack(
+        [
+            np.eye(4),
+        ]
+        * target_to_cam_rotation.shape[0]
+    )
+    target_to_cam_T[:, :3, :3] = target_to_cam_rotation
+    target_to_cam_T[:, :3, -1] = target_to_cam_translation
 
 
 if __name__ == "__main__":
